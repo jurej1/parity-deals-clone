08bace2480c3ba73ea8051e8046b50d7e459097f
Jure Jures <jure.04jj@gmail.com>
2025-04-22 22:23:35 +0200

    addinc cache.

--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,23 +1,30 @@
 import { db } from "@/drizzle/db";
 import { ProductCustomizationTable, ProductTable } from "@/drizzle/schema";
+import {
+  CACHE_TAGS,
+  dbCache,
+  getUserTag,
+  revalidateDbCache,
+} from "@/lib/cache";
 import { eq, and } from "drizzle-orm";
+import { revalidateTag } from "next/cache";
 
 export function getProducts(userId: string, { limit }: { limit?: number }) {
-  // give me all products where this user ID matches clerk_user_id
-  return db.query.ProductTable.findMany({
-    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
-    orderBy: ({ createdAt }, { desc }) => desc(createdAt),
+  // when making a get request, tag it as specific as possible
+  const cacheFn = dbCache(getProductsInternal, {
+    tags: [getUserTag(userId, CACHE_TAGS.products)],
   });
+
+  return cacheFn(userId, { limit });
 }
 
 export async function createProduct(data: typeof ProductTable.$inferInsert) {
-  // inserting the produt and returning the product id.
   const [newProduct] = await db.insert(ProductTable).values(data).returning({
     id: ProductTable.id,
+    userId: ProductTable.clerkUserId,
   });
 
   try {
-    // if the product Id already exists -> do nothing
     await db
       .insert(ProductCustomizationTable)
       .values({
@@ -27,9 +34,15 @@ export async function createProduct(data: typeof ProductTable.$inferInsert) {
         target: ProductCustomizationTable.productId,
       });
   } catch (error) {
-    // deleting the product in case of error when creating customization row
     await db.delete(ProductTable).where(eq(ProductTable.id, newProduct.id));
   }
+
+  // revalidates my cache information
+  revalidateDbCache({
+    tag: CACHE_TAGS.products,
+    userId: newProduct.userId,
+    id: newProduct.id,
+  });
 
   return newProduct;
 }
@@ -39,5 +52,22 @@ export async function deleteProduct(id: string, userId: string) {
     .delete(ProductTable)
     .where(and(eq(ProductTable.id, id), eq(ProductTable.clerkUserId, userId)));
 
+  if (rowCount > 0) {
+    // revalidates my cache information
+    revalidateDbCache({
+      tag: CACHE_TAGS.products,
+      userId: userId,
+      id: id,
+    });
+  }
+
   return rowCount > 0;
+}
+
+// does the actual call for us
+function getProductsInternal(userId: string, { limit }: { limit?: number }) {
+  return db.query.ProductTable.findMany({
+    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
+    orderBy: ({ createdAt }, { desc }) => desc(createdAt),
+  });
 }
