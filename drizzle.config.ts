23910b67002f39d6b932ef371f87ad9b8af2f408
Jure Jures <jure.04jj@gmail.com>
2025-04-22 10:24:17 +0200

    drizzle table crated.

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { env } from "@/data/env/server";
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./src/drizzle",
+  schema: "./src/drizzle/schema.ts",
+  dialect: "postgresql",
+  strict: true,
+  verbose: true,
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+});
